7dd4e9b35d6c6566e8a37373aa9e88b76e0861d8
Shalu99v <shalu99v@gmail.com>
2025-11-21 18:05:34 +0530

    url chnaged

--- Frontend/components/BookingForm.tsx
+++ Frontend/components/BookingForm.tsx
@@ -77,7 +77,7 @@ export default function BookingForm() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:4000/bookings", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/bookings`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
